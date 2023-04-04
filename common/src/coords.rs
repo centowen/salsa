72f7299c000f894a9155692a4c81ccba3f4cf40e
Eskil Varenius <eskil.varenius@gmail.com>
2023-04-04 17:00:48 +0200

    Varenius/more coordinates (#41)
    * Replace 'now' with 'when' internally
    
    * Added calculations for Sun position in ecliptic, equatorial and horizontal coordinates + test
    
    * Added ecliptic-->equatorial conversion
    
    * Added vlsr-correction calculation and required coordinate conversion functions. Removed unused functions. Cleanup and formatting.
    
    * rustfmt again, manually edited wrong during review...need to learn rust better!

--- common/src/coords.rs
+++ common/src/coords.rs
@@ -2,6 +2,14 @@ use crate::{Direction, Location};
 use chrono::prelude::*;
 use std::f64::consts::PI;
 
+// Obliquity of the ecliptic, accurate to 1 arcmin per century from J2000
+const EC: f64 = 0.40909260052;
+// The generally accepted motion of the Sun in the LSR is that it is moving at a speed of about
+// 20 km/s towards an “apex” that is rather close to the direction of Vega, with coordinates
+// R.A. = 18 hr = 270 deg, and Dec. = 30 deg. Define this apex as ARA, ADE:
+const ARA: f64 = 4.71238898038;
+const ADE: f64 = 0.52359877559;
+
 fn julian_day(when: DateTime<Utc>) -> f64 {
     // Calculate decimal julian day for specified date. We can simplify
     // since we do not need to cover dates in the past, only the future!
@@ -90,14 +98,18 @@ pub fn horizontal_from_galactic(
     horizontal_from_equatorial(location, when, ra, dec)
 }
 
-fn equatorial_from_ecliptic(l: f64, b: f64) -> (f64, f64) {
-    // From https://astrophysicsandpython.com/2021/04/19/celestial-coordinate-conversions/#Equatorial_Ecliptic
-    let _ecliptic = 23.43927944_f64.to_radians();
-    let dec = (b.sin() * _ecliptic.cos() + b.cos() * _ecliptic.sin() * l.sin()).asin();
-    let ra =
-        (b.cos() * l.sin() * _ecliptic.cos() - b.sin() * _ecliptic.sin()).atan2(b.cos() * l.cos());
-    (ra, dec)
+fn ecliptic_from_equatorial(ra: f64, dec: f64) -> (f64, f64) {
+    // From javascript code behind calculations at https://frostydrew.org/utilities.dc/convert/tool-eq_coordinates/
+    let l = (ra.tan() * EC.cos() + dec.tan() * EC.sin() / ra.cos()).atan();
+    let b = (dec.sin() * EC.cos() - dec.cos() * EC.sin() * ra.sin()).asin();
+    (l, b)
 }
+
+//fn ecliptic_from_galactic(gl: f64, gb: f64) -> (f64, f64) {
+//    let (ra, dec) = equatorial_from_galactic(gl, gb);
+//    let (l, b) = ecliptic_from_equatorial(ra, dec);
+//    (l, b)
+//}
 
 fn ecliptic_from_sun(when: DateTime<Utc>) -> (f64, f64) {
     // Algorithm from https://aa.usno.navy.mil/faq/sun_approx
@@ -134,6 +146,31 @@ pub fn horizontal_from_sun(location: Location, when: DateTime<Utc>) -> Direction
     horizontal_from_equatorial(location, when, ra, dec)
 }
 
+pub fn vlsrcorr_from_galactic(l: f64, b: f64, when: DateTime<Utc>) -> f64 {
+    // From http://web.mit.edu/8.13/www/srt_software/vlsr.pdf
+
+    // Target direction in equatorial coordinates
+    let (ra, dec) = equatorial_from_galactic(l, b);
+
+    // Movement of Sun with respect to LSR: dot product of target & apex vectors, in km/s
+    let vsun = 20.0
+        * (ARA.cos() * ADE.cos() * ra.cos() * dec.cos()
+            + ARA.sin() * ADE.cos() * ra.sin() * dec.cos()
+            + ADE.sin() * dec.sin());
+
+    // Target direction in ecliptic coordinates
+    let (tl, tb) = ecliptic_from_equatorial(ra, dec);
+
+    // Sun direction in ecliptic coordinates
+    let (sl, _sb) = ecliptic_from_sun(when);
+
+    // Calculate correction due to earth movement relative to the Sun, in km/s
+    let vorb = 30.0 * tb.cos() * (sl - tl).sin();
+
+    // return total correction in m/s
+    1e3 * (vsun + vorb)
+}
+
 #[cfg(test)]
 mod test {
     use chrono::Duration;
@@ -168,6 +205,7 @@ mod test {
             1e-6
         );
     }
+
     #[test]
     fn test_horizontal_from_sun() {
         // Test that we get the correct horizontal position for the Sun
@@ -184,5 +222,17 @@ mod test {
         let expected_alt = 0.6557470215389855;
         assert_similar!(dir.azimuth, expected_az, 1e-6);
         assert_similar!(dir.altitude, expected_alt, 1e-6);
+    }
+
+    #[test]
+    fn test_vlsrcorr_from_galactic() {
+        // Test that we get the correct horizontal position for the Sun
+        // given specific location and time
+        let jdref = Utc.with_ymd_and_hms(2023, 4, 4, 15, 0, 0).unwrap();
+        let glon = 140.0_f64.to_radians();
+        let vlsrcorr = vlsrcorr_from_galactic(glon, 0.0, jdref);
+        // Expected horizontal coordinates in radians
+        let expected_vlsrcorr = -15443.385967834394;
+        assert_similar!(vlsrcorr, expected_vlsrcorr, 1e-6);
     }
 }
