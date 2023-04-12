a0d0d66c524330ee85ec1be29d85e7cb69c5157b
Magnus Sandén <thefreid@gmail.com>
2023-04-12 09:29:43 +0200

    Booking form with submit callback
    The callback extracts the values from the fields and performs a POST
    request to add the new booking to the database.
    
    The datetimes need to be entered in exactly this format:
    1996-12-19T16:39:57-08:00
    
    There is a lot of stuff left to do:
    
    - Handle errors in the submit callback
      (remove unwraps and shows errors to the user)
    - Better UI for entering dates/times
    - Better UI for selecting telescope (incl. "Any available")
    - Check availability of selected time slot
    - User authentication

--- frontend/src/pages/bookings.rs
+++ frontend/src/pages/bookings.rs
@@ -1,8 +1,84 @@
 use common::Booking;
 use gloo_net::http::Request;
+use web_sys::HtmlInputElement;
 use yew::platform::spawn_local;
 use yew::prelude::*;
 use yew::{html, Context};
+fn value_from_ref(node_ref: &NodeRef) -> Option<String> {
+    node_ref.cast::<HtmlInputElement>().map(|e| e.value())
+}
+
+#[function_component(NewBooking)]
+fn new_booking() -> Html {
+    let telescope_ref = use_node_ref();
+    let start_time_ref = use_node_ref();
+    let end_time_ref = use_node_ref();
+
+    let onclick = {
+        let telescope_ref = telescope_ref.clone();
+        let start_time_ref = start_time_ref.clone();
+        let end_time_ref = end_time_ref.clone();
+
+        Callback::from(move |event: SubmitEvent| {
+            event.prevent_default();
+            log::info!("Submit!");
+
+            let telescope_name = value_from_ref(&telescope_ref).unwrap();
+            let start_time = value_from_ref(&start_time_ref).unwrap();
+            let end_time = value_from_ref(&end_time_ref).unwrap();
+            log::info!("Telescope: {:?}", telescope_name);
+            log::info!("Start time: {:?}", start_time);
+            log::info!("End time: {:?}", end_time);
+
+            let booking = Booking {
+                telescope_name,
+                start_time: start_time.parse().unwrap(),
+                end_time: end_time.parse().unwrap(),
+                user_name: "Anonymous".to_string(),
+            };
+
+            spawn_local(async move {
+                match Request::post("/api/booking")
+                    .json::<Booking>(&booking)
+                    .unwrap()
+                    .send()
+                    .await
+                {
+                    Ok(response) => {
+                        log::info!("Got response: {:?}", response);
+                        let value = response
+                            .json::<usize>()
+                            .await
+                            .expect("Failed to deserialize response");
+                        log::info!("Got response value: {:?}", value);
+                    }
+                    Err(error) => {
+                        log::error!("Failed to fetch: {}", error);
+                    }
+                }
+            });
+        })
+    };
+
+    html!(
+        <div class="new-booking">
+            <form id="new-booking-form" method="get" onsubmit={ onclick }>
+                <label for="telescope">{ "Telescope:" }</label>
+                <select name="telescope" ref={ telescope_ref } id="telescope">
+                    <option value="">{ "Any telescope" }</option>
+                    <option value="brage">{ "Brage" }</option>
+                    <option value="vale">{ "Vale" }</option>
+                    <option value="torre">{ "Torre" }</option>
+                </select>
+                <label for="start-time">{ "Start time:" }</label>
+                <input type="text" ref={ start_time_ref } id="start-time" name="start-time" />
+                <label for="end-time">{ "End time:" }</label>
+                <input type="text" ref={ end_time_ref } id="end-time" name="end-time" />
+                <input type="submit" value="Submit" />
+            </form>
+        </div>
+    )
+}
 
 pub enum Message {
     ReceiveBookings(Vec<Booking>),
@@ -61,8 +137,11 @@ impl Component for BookingsPage {
             })
             .collect::<Html>();
         html! {
-            <div class="bookings">
-                { bookings }
+            <div>
+                <div class="bookings">
+                    { bookings }
+                </div>
+                <NewBooking />
             </div>
         }
     }
