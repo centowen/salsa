5de6930c1b4ca27224aea2aed5ec6bc46a61bab8
Jonathan Karlsson <karlsjona@gmail.com>
2023-07-02 23:53:38 +0200

    Receive data and send ACK back

--- fakesalsa/src/main.rs
+++ fakesalsa/src/main.rs
@@ -1,5 +1,36 @@
-use std::net::TcpListener;
+use std::io::prelude::*;
+use std::net::{TcpListener, TcpStream};
 use std::process;
+use hex_literal::hex;
+
+fn controller_connection(mut stream: TcpStream) {
+    loop {
+        let mut command_buffer = [0; 13];
+        match stream.read(&mut command_buffer) {
+            Ok(0) => {
+                println!("Client closed connection.");
+                break;
+            }
+            Ok(13) => {
+                println!("Client sent: {:02X?}", command_buffer);
+                // FIXME: Send proper response based on sent command
+                println!("Sending ACK");
+                // FIXME: Error handling
+                stream.write(&hex!("57000000000000000000000020")).unwrap();
+            }
+            Ok(n) => {
+                println!(
+                    "Client sent {} bytes, expected 13. Data: {:02X?}",
+                    n, command_buffer
+                );
+            }
+            _ => {
+                // FIXME: Handle these errors more gracefully.
+                println!("Something went wrong!");
+            }
+        };
+    }
+}
 
 fn main() {
     let address = "127.0.0.1:3001";
@@ -10,7 +41,12 @@ fn main() {
             process::exit(1);
         }
     };
-    for _ in listener.incoming() {
-        break;
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => controller_connection(stream),
+            Err(err) => {
+                println!("Failed to accept connection ({})", err);
+            }
+        }
     }
 }
