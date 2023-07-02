d8afdfcae6772743a401d94eb623e0c2b22f38b7
Jonathan Karlsson <karlsjona@gmail.com>
2023-07-02 23:15:45 +0200

    Accept a TCP connection

--- fakesalsa/src/main.rs
+++ fakesalsa/src/main.rs
@@ -1,3 +1,16 @@
+use std::net::TcpListener;
+use std::process;
+
 fn main() {
-    println!("Hello, world!");
+    let address = "127.0.0.1:3001";
+    let listener = match TcpListener::bind(address) {
+        Ok(listener) => listener,
+        Err(err) => {
+            println!("Failed to bind to address {} ({})", address, err);
+            process::exit(1);
+        }
+    };
+    for _ in listener.incoming() {
+        break;
+    }
 }
