e8af7876ccf61151dd95c6c533f2fd9973c5c52f
Magnus Sandén <thefreid@gmail.com>
2023-09-20 22:40:55 +0200

    Update welcome page copy

--- frontend/src/pages/welcome.rs
+++ frontend/src/pages/welcome.rs
@@ -4,8 +4,11 @@ use yew::prelude::*;
 pub fn welcome_page() -> Html {
     html! {
         <div class="welcome">
-            <h1>{ "Observe the galaxy" }</h1>
-            <h3>{ "with a real radio telescope" }</h3>
+            // <h1>{ "Observe the Milky Way" }</h1>
+            <h1>{ "Radio astronomy in your browser" }</h1>
+            <div><p>{ "The Salsa telescopes are available right here in your
+            browser. Do real radio observations of our home, the Milky Way."
+            }</p></div>
             // "Try it now!" Button which leads to the observation page?
         </div>
     }
