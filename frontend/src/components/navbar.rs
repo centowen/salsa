4df4300aa4d09b066f38ad750205e6a6b2461ceb
Magnus Sandén <thefreid@gmail.com>
2023-09-26 22:47:17 +0200

    Responsive navbar
    Switches to toggled burger menu at 768px.
    
    TODO: Sizes, alignments, paddings and stuff.

--- frontend/src/components/navbar.rs
+++ frontend/src/components/navbar.rs
@@ -5,31 +5,42 @@ use crate::Route;
 
 #[function_component(NavBar)]
 pub fn navbar() -> Html {
+    let hide_menu = use_state(|| true);
+
+    let toggle_menu = Callback::from({
+        let hide_menu = hide_menu.clone();
+        move |_| {
+            hide_menu.set(!*hide_menu);
+        }
+    });
+
     html! {
-        <>
-            <nav>
+        <header>
+            <div class="logo-burger">
+                <div class="logo"><Link<Route> to={Route::Home}>{"SALSA"}</Link<Route>></div>
+                <a href="#" onclick={ toggle_menu } class="burger"><i class="fa fa-bars" /></a>
+            </div>
+            <nav class={ classes!(hide_menu.then_some("hide-menu")) }>
                 <menu>
-                    <li class="list-entry">
+                    <li>
                         <Link<Route> to={Route::Observe}>{ "Observe" }</Link<Route>>
                     </li>
-                    <li class="list-entry">
+                    <li>
                         <Link<Route> to={Route::Bookings}>{ "Bookings" }</Link<Route>>
                     </li>
-                    <li class="list-entry">
+                    <li>
                         <Link<Route> to={Route::MakeBooking}>{ "Make booking" }</Link<Route>>
                     </li>
-                    <li class="list-entry">
+                    <li>
                         <Link<Route> to={Route::Weather}>{ "Weather" }</Link<Route>>
                     </li>
                 </menu>
             </nav>
-            <nav>
+            <nav class={ classes!(hide_menu.then_some("hide-menu")) }>
                 <menu>
-                    <li class="list-entry">
-                        <a href="#">{ "Login" }</a>
-                    </li>
+                    <li><a href="#">{ "Login" }</a></li>
                 </menu>
             </nav>
-        </>
+        </header>
     }
 }
