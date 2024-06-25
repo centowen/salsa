e874895f2abd08871e8c76359091c65a54f8027c
Jonathan Karlsson <karlsjona@gmail.com>
2024-06-25 23:21:34 +0200

    Fill in more telescope status in observe

--- backend/src/observe_routes.rs
+++ backend/src/observe_routes.rs
@@ -5,10 +5,11 @@ use axum::extract::State;
 use axum::http::StatusCode;
 use axum::response::{IntoResponse, Response};
 use axum::{routing::get, Router};
-use common::{Direction, TelescopeError};
+use common::{Direction, TelescopeError, TelescopeInfo};
 
 pub fn routes(telescopes: TelescopeCollection) -> Router {
-    Router::new().route("/", get(get_observe))
+    Router::new()
+        .route("/", get(get_observe))
         .with_state(telescopes)
 }
 
@@ -24,13 +25,17 @@ impl From<TelescopeError> for TelescopeNotFound {
     fn from(_: TelescopeError) -> Self {
         TelescopeNotFound {}
     }
-    }
+}
 
 #[derive(Template)]
 #[template(path = "observe.html")]
 struct ObserveTemplate {
-    telescope_name: String,
+    info: TelescopeInfo,
+    status: String,
+    target_mode: String,
     direction: Direction,
+    commanded_x: f64,
+    commanded_y: f64,
 }
 
 async fn get_observe(
@@ -40,8 +45,31 @@ async fn get_observe(
     let telescope = telescopes.get("fake").ok_or(TelescopeNotFound)?;
     let telescope = telescope.telescope.clone().lock_owned().await;
     let info = telescope.get_info().await?;
+    let target_mode = match &info.current_target {
+        common::TelescopeTarget::Equatorial { .. } => "equatorial",
+        common::TelescopeTarget::Galactic { .. } => "galactic",
+        common::TelescopeTarget::Parked => "equatorial",
+        common::TelescopeTarget::Stopped => "equatorial",
+    }
+    .to_string();
+    let (commanded_x, commanded_y) = match info.current_target {
+        common::TelescopeTarget::Equatorial { ra, dec } => (ra, dec),
+        common::TelescopeTarget::Galactic { l, b } => (l, b),
+        _ => (
+            info.current_horizontal.azimuth,
+            info.current_horizontal.altitude,
+        ),
+    };
     Ok(HtmlTemplate(ObserveTemplate {
-        telescope_name: info.id,
+        info: info.clone(),
+        status: match &info.status {
+            common::TelescopeStatus::Idle => "Idle".to_string(),
+            common::TelescopeStatus::Slewing => "Slewing".to_string(),
+            common::TelescopeStatus::Tracking => "Tracking".to_string(),
+        },
+        target_mode,
         direction: telescope.get_direction().await?,
+        commanded_x,
+        commanded_y,
     }))
 }
