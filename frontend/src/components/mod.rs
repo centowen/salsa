365fd805bed4b27b3b203c2e2a54b903c7792254
Magnus Sandén <freidrichen@users.noreply.github.com>
2024-07-12 23:06:07 +0200

    Remove frontend folder (#92)

--- frontend/src/components/mod.rs
+++
@@ -1,4 +0,0 @@
-pub mod graph;
-pub mod navbar;
-pub mod notification_area;
-pub mod target_selector;
--- frontend/src/pages/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod bookings;
-pub mod make_booking;
-pub mod observe;
-pub mod telescope;
-pub mod weather;
-pub mod welcome;
