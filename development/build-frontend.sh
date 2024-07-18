0c7e59484f1aba371090dcc7424984a12b6b85d2
Magnus Sandén <thefreid@gmail.com>
2024-07-18 14:30:27 +0200

    Update development folder
    Most stuff is removed since there no longer is a frontend.

--- development/build-frontend.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-cd frontend && mkdir -p ../artifacts/frontend/salsa && trunk build --public-url salsa/ --release -d ../artifacts/frontend/salsa && cd ..
-export FRONTEND_PATH=$(pwd)/artifacts/frontend
--- development/serve-frontend.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-cd frontend && \
-    trunk serve --public-url salsa/ \
-    --proxy-backend http://localhost:3000/api/ \
-    --proxy-rewrite /api/
