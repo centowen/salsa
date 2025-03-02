51e75c055abba9ea00aa8dd1a7471ff6b7296923
Jonathan Karlsson <karlsjona@gmail.com>
2025-03-02 14:36:31 +0100

    Render index completely on server side (#98)
    This is a preparation for rendering pages as fragments for htmx updates
    of as complete pages.

--- src/index.rs
+++ src/index.rs
@@ -1,11 +1,25 @@
+use std::fs::read_to_string;
+
 use crate::template::HtmlTemplate;
 use askama::Template;
-use axum::response::IntoResponse;
+use axum::response::{Html, IntoResponse, Response};
 
 #[derive(Template)]
-#[template(path = "index.html")]
-struct IndexTemplate {}
+#[template(path = "index.html", escape = "none")]
+struct IndexTemplate {
+    content: String,
+}
 
-pub async fn get_index() -> impl IntoResponse {
-    HtmlTemplate(IndexTemplate {})
+pub async fn get_index() -> Response {
+    // TODO: Read this file at startup.
+    Html(render_main(
+        read_to_string("assets/welcome.html").expect("Reading static data should always work"),
+    ))
+    .into_response()
+}
+
+pub fn render_main(content: String) -> String {
+    IndexTemplate { content }
+        .render()
+        .expect("Template should always succeed")
 }
